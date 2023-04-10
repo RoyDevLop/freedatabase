d4bfec3a8d1fad552344cf2ce1c199eddbe3b24f
Royer Avalos <104845417+RoyDevLop@users.noreply.github.com>
2023-04-10 09:47:26 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       '/posts': "https://serverbackend-br77.onrender.com" ,
       '/register': "https://serverbackend-br77.onrender.com" ,
       '/login': "https://serverbackend-br77.onrender.com",
-      '/users': "https://serverbackend-br77.onrender.com",  // THIS IS BACKEND'S URL,  THE FRONTEND'S URL IS FROM http://localhost:5173
+      '/users': "https://serverbackend-br77.onrender.com",
       '/users/:id': "https://serverbackend-br77.onrender.com",
       '/forgotPassword' : "https://serverbackend-br77.onrender.com",
       '/codigo-contrasena': "https://serverbackend-br77.onrender.com",
